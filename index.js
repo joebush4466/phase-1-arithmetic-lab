ac6d59048e1a2157ae60cd0b4c8b69d69c5b09a3
Joe Bush <joebush4444@gmail.com>
2024-04-05 16:44:07 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = (num1 * num2);
+const random = Math.round(Math.random()+Math.random());
+let num3 = 24
+let num4 = 20
+let mod = (num3%num4);
+let max = Math.max(1,2,3,4,5,11,12,13,14,15,20);
